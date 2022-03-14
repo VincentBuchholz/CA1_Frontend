a90c621db84e38df77cf106b8430f0d9e8687221
Vincent Buchholz <vincbuchholz@gmail.com>
2022-03-14 10:51:33 +0100

    adds citiy facade and get method for all cities

---
+++ citiesFacade.js
@@ -0,0 +1,36 @@
+
+const URL = "https://vincehub.dk/tomcat/ca1/api/person/"
+
+
+function getCities() {
+    return fetch(URL+"zip/all")
+        .then(res => res.json())
+}
+
+function makeOptions(method, body) {
+    var opts =  {
+        method: method,
+        headers: {
+            "Content-type": "application/json",
+            "Accept": "application/json"
+        }
+    }
+    if(body){
+        opts.body = JSON.stringify(body);
+    }
+    return opts;
+}
+
+
+function handleHttpErrors(res){
+    if(!res.ok){
+        return Promise.reject({status: res.status, fullError: res.json() })
+    }
+    return res.json();
+}
+
+const citiesFacade ={
+    getCities
+}
+
+export default citiesFacade;
--- main.js
+++ main.js
@@ -1,5 +1,55 @@
 import './style.css'
 import "bootstrap/dist/css/bootstrap.css"
+import citiesFacade from "./citiesFacade";
+
+//JS for cities
+let cities = citiesFacade.getCities().then(cities =>{
+    const cityRows = cities.map(city => `
+<tr>
+    <td>${city.zip}</td>
+    <td>${city.city}</td>
+</tr>`);
+
+    document.querySelector("#citiesTable").innerHTML = cityRows.join("");
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function hideAllShowOne(idToShow)
+{
+    document.getElementById("startPage").style = "display:none"
+    document.getElementById("citiesPage").style = "display:none"
+    document.getElementById("personsPage").style = "display:none"
+    document.getElementById("hobbiesPage").style = "display:none"
+    document.getElementById(idToShow).style = "display:block"
+}
+
+function menuItemClicked(evt)
+{
+    const id = evt.target.id;
+    switch (id)
+    {
+        case "cities": hideAllShowOne("citiesPage"); break
+        case "persons": hideAllShowOne("personsPage"); break
+        case "hobbies": hideAllShowOne("hobbiesPage"); break
+        default: hideAllShowOne("startPage"); break
+    }
+    evt.preventDefault();
+}
+document.getElementById("menu").onclick = menuItemClicked;
+hideAllShowOne("startPage");
 
 
 import "bootstrap/dist/js/bootstrap.js"
